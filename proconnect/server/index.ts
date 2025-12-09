a00490c7f478ed0b255d358d0f70867f9afbe519
Peter-ConX <okaforchi5762@gmail.com>
2025-12-09 21:48:41 +0100

    Delete proconnect directory

--- proconnect/server/index.ts
+++
@@ -1,5 +0,0 @@
-// Socket.IO server setup
-// Note: For production, you may want to run this as a separate service
-// or integrate it with your Next.js server differently
-
-export { initializeSocket } from './socket'
